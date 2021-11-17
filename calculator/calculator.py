643e8e2b7348e9bb8caf1ce2da4046d6379a514d
Rafael Guterres Jeffman <rafasgj@gmail.com>
2021-11-17 19:27:04 -0300

    uri1001: Add domain level implementation.

---
+++ calculator/calculator.py
@@ -0,0 +1,34 @@
+# This file is part of gerkin-by-example
+#
+# Copyright (C) 2021 Rafael Guterres Jeffman
+#
+# This software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Implement the domain."""
+
+
+class Calculator:
+    """A helper for math novices."""
+
+    def __init__(self):
+        self.numbers = []
+
+    def add_input(self, value):
+        """Add a new input to the calculator."""
+        self.numbers.append(value)
+
+    def sum(self):
+        a = self.numbers.pop()
+        b = self.numbers.pop()
+        return a + b
