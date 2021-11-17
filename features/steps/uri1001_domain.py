807a93b71910c457b3a82b6a1d3154bc252dac39
Rafael Guterres Jeffman <rafasgj@gmail.com>
2021-11-17 19:26:20 -0300

    uri1001: Add test steps for domain level feature.

---
+++ features/steps/uri1001_domain.py
@@ -0,0 +1,36 @@
+# This file is part of gerkin-by-example
+#
+# Copyright (C) 2021 Rafael Guterres Jeffman
+#
+# This software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <https://www.gnu.org/licenses/>.
+
+from behave import given, then, when
+
+
+@given("the first number is {number:d}")
+def _given_first_number(context, number):
+    context.calculator.add_input(number)
+
+
+@given("the second number is {number:d}")
+def _given_second_number(context, number):
+    context.calculator.add_input(number)
+
+@when("the two numbers are added")
+def _when_two_numbers_are_added(context):
+    context.result = context.calculator.sum()
+
+@then("the result should be {result:d}")
+def step_impl(context, result):
+    assert context.result == result
